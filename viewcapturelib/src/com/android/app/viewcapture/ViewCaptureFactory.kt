934601ec6656b47a3098b1028dbdaf29cd9a6896
Liana Kazanova (xWF) <lkazanova@google.com>
2024-10-11 17:09:58 +0000

    Revert "Remove main thread necessity for getting a ViewCapture instance and make"
    This reverts commit ebfbc9168bc46b9be0c793c47550e5732f2ebb36.
    
    Reason for revert:DroidMonitor: Potential culprit for http://b/372910511 - verifying through ABTD before revert submission. This is part of the standard investigation process, and does not mean your CL will be reverted.
    
    Change-Id: I7e5523f95f50919d2a572bad4c1d2821c1622191

--- viewcapturelib/src/com/android/app/viewcapture/ViewCaptureFactory.kt
+++ viewcapturelib/src/com/android/app/viewcapture/ViewCaptureFactory.kt
@@ -17,6 +17,7 @@
 package com.android.app.viewcapture
 
 import android.content.Context
+import android.os.Looper
 import android.os.Process
 import android.tracing.Flags
 import android.util.Log
@@ -26,64 +27,62 @@ import android.view.WindowManager
  * Factory to create polymorphic instances of ViewCapture according to build configurations and
  * flags.
  */
-object ViewCaptureFactory {
-    private val TAG = ViewCaptureFactory::class.java.simpleName
-    private val instance: ViewCapture by lazy { createInstance() }
-    private lateinit var appContext: Context
+class ViewCaptureFactory {
+    companion object {
+        private val TAG = ViewCaptureFactory::class.java.simpleName
+        private var instance: ViewCapture? = null
 
-    private fun createInstance(): ViewCapture {
-        return when {
-            !android.os.Build.IS_DEBUGGABLE -> {
-                Log.i(TAG, "instantiating ${NoOpViewCapture::class.java.simpleName}")
-                NoOpViewCapture()
+        @JvmStatic
+        fun getInstance(context: Context): ViewCapture {
+            if (Looper.myLooper() != Looper.getMainLooper()) {
+                return ViewCapture.MAIN_EXECUTOR.submit { getInstance(context) }.get()
             }
-            !Flags.perfettoViewCaptureTracing() -> {
-                Log.i(TAG, "instantiating ${SettingsAwareViewCapture::class.java.simpleName}")
-                SettingsAwareViewCapture(
-                    appContext,
-                    ViewCapture.createAndStartNewLooperExecutor(
-                        "SAViewCapture",
-                        Process.THREAD_PRIORITY_FOREGROUND,
-                    ),
-                )
+
+            if (instance != null) {
+                return instance!!
             }
-            else -> {
-                Log.i(TAG, "instantiating ${PerfettoViewCapture::class.java.simpleName}")
-                PerfettoViewCapture(
-                    appContext,
-                    ViewCapture.createAndStartNewLooperExecutor(
-                        "PerfettoViewCapture",
-                        Process.THREAD_PRIORITY_FOREGROUND,
-                    ),
-                )
-            }
+
+            return when {
+                !android.os.Build.IS_DEBUGGABLE -> {
+                    Log.i(TAG, "instantiating ${NoOpViewCapture::class.java.simpleName}")
+                    NoOpViewCapture()
+                }
+                !Flags.perfettoViewCaptureTracing() -> {
+                    Log.i(TAG, "instantiating ${SettingsAwareViewCapture::class.java.simpleName}")
+                    SettingsAwareViewCapture(
+                        context.applicationContext,
+                        ViewCapture.createAndStartNewLooperExecutor(
+                            "SAViewCapture",
+                            Process.THREAD_PRIORITY_FOREGROUND
+                        )
+                    )
+                }
+                else -> {
+                    Log.i(TAG, "instantiating ${PerfettoViewCapture::class.java.simpleName}")
+                    PerfettoViewCapture(
+                        context.applicationContext,
+                        ViewCapture.createAndStartNewLooperExecutor(
+                            "PerfettoViewCapture",
+                            Process.THREAD_PRIORITY_FOREGROUND
+                        )
+                    )
+                }
+            }.also { instance = it }
         }
-    }
 
-    /** Returns an instance of [ViewCapture]. */
-    @JvmStatic
-    fun getInstance(context: Context): ViewCapture {
-        synchronized(this) {
-            if (this::appContext.isInitialized && appContext != context.applicationContext) {
-                throw IllegalStateException("appContext is already set to a different value")
-            }
-            appContext = context.applicationContext
+        /** Returns an instance of [ViewCaptureAwareWindowManager]. */
+        @JvmStatic
+        fun getViewCaptureAwareWindowManagerInstance(
+            context: Context,
+            isViewCaptureTracingEnabled: Boolean
+        ): ViewCaptureAwareWindowManager {
+            val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
+            val lazyViewCapture = lazy { getInstance(context) }
+            return ViewCaptureAwareWindowManager(
+                windowManager,
+                lazyViewCapture,
+                isViewCaptureTracingEnabled
+            )
         }
-        return instance
-    }
-
-    /** Returns an instance of [ViewCaptureAwareWindowManager]. */
-    @JvmStatic
-    fun getViewCaptureAwareWindowManagerInstance(
-        context: Context,
-        isViewCaptureTracingEnabled: Boolean,
-    ): ViewCaptureAwareWindowManager {
-        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager
-        val lazyViewCapture = lazy { getInstance(context) }
-        return ViewCaptureAwareWindowManager(
-            windowManager,
-            lazyViewCapture,
-            isViewCaptureTracingEnabled,
-        )
     }
 }
