554edf1afd360d72d5161871efd4856a890a9b3b
Peter Kalauskas <peskal@google.com>
2024-11-01 18:12:41 +0000

    tracinglib: apply latest kt format style
    Apply changes from latest version of ktfmt
    
    Test: m
    Flag: EXEMPT ktfmt
    Merged-In: I10aef45bca433669820db8ad42b9e1e67c85d283
    Change-Id: I10aef45bca433669820db8ad42b9e1e67c85d283

--- tracinglib/demo/src/com/android/app/tracing/demo/MainActivity.kt
+++ tracinglib/demo/src/com/android/app/tracing/demo/MainActivity.kt
@@ -58,7 +58,7 @@ class MainActivity : Activity() {
                 context.getString(
                     R.string.run_experiment_button_text,
                     className,
-                    demo.getDescription()
+                    demo.getDescription(),
                 )
             setOnClickListener {
                 val experimentName = "$className #${launchCounter++}"
@@ -71,7 +71,7 @@ class MainActivity : Activity() {
                             Trace.TRACE_TAG_APP,
                             TRACK_NAME,
                             "Running $experimentName",
-                            it.hashCode()
+                            it.hashCode(),
                         )
                     }
                 }
